3cc322ace7340c123f1693e73dcfe849caa80e4a
jawnior <jakub.jawniak@gmail.com>
2023-02-11 19:11:51 +0100

    2 new projects added

--- website.js
+++ website.js
@@ -1,13 +1,35 @@
-let textboxParagraph = document.getElementById('geodeticWebsite')
-let image = document.getElementById('geodeticWebsitePicture');
-let imageVisible = false;
+let geodeticWebsite = document.getElementById('geodeticWebsite');
+let geodeticWebsitePicture = document.getElementById('geodeticWebsitePicture');
 
-textboxParagraph.addEventListener("click", function () {
-    if (imageVisible) {
-        image.style.display = 'none';
-        imageVisible = false;
+let colorFlipper = document.getElementById('colorFlipper');
+let colorFlipperPicture = document.getElementById('colorFlipperPicture');
+
+let counter = document.getElementById('counter');
+let counterPicture = document.getElementById('counterPicture');
+
+let currentPicture = null;
+
+function toggleImageVisibility(element) {
+    if (currentPicture === element) {
+        element.style.display = 'none';
+        currentPicture = null;
     } else {
-        image.style.display = 'block';
-        imageVisible = true;
+        if (currentPicture) {
+            currentPicture.style.display = 'none';
+        }
+        element.style.display = 'block';
+        currentPicture = element;
     }
+}
+
+geodeticWebsite.addEventListener("click", function () {
+    toggleImageVisibility(geodeticWebsitePicture);
+});
+
+colorFlipper.addEventListener("click", function () {
+    toggleImageVisibility(colorFlipperPicture);
+});
+
+counter.addEventListener("click", function () {
+    toggleImageVisibility(counterPicture);
 });
